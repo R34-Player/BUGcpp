1fe2f47fbdd379b302c015e96fac423ebd0d1405
Hector_alv_ <76228601+R34-Player@users.noreply.github.com>
2022-03-23 11:55:48 -0600

    Add files via upload

---
+++ Tarea2.0(solucionado).cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+int opc,a;
+char nombre[50];
+int main(){
+    do
+    {
+    	system("cls");
+        cout<<"Nacimiento de alan turing	";
+        cin>>a;
+
+        if(a != 1912){
+            cout<<"Incorrecto, adios";
+            exit(0);
+        }
+        else{
+            cout<<"Correcto";
+            cin.getline(nombre,50,'\n');
+			// se saltaba uno si no lo ponia como si pidiera
+        }
+        cout<<"\nIngresa tu nombre: ";
+        cin.getline(nombre,50,'\n');
+        cout<<"\nIngresa donde estudias:    ";
+        cin.getline(nombre,50,'\n');
+        cout<<"\nIngresa que carrera te gustaria estudiar:   ";
+        cin.getline(nombre,50,'\n');
+        do{
+            system("cls");
+            cout<<"quieres conitnuar?\n";
+            cout<<"\n   1-SI";
+            cout<<"\n   2-NO";
+            cout<<"\nRespuesta: ";
+            cin>>opc;
+        }
+        while(opc >= 3);
+        
+
+    }
+    while(opc==1);
+    return 0;
+}
